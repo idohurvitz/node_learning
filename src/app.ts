e85c6ae21550b8ace66a8d69ed2e2f37db645509
Ido <idoh52@gmail.com>
2022-01-19 21:52:26 +0200

    using routes and new logger

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express from 'express';
+import routes from './routes';
+import logger from './utils/logger';
 const port = 3001;
 const hostname = 'localhost';
 
 const app = express()
-app.listen(port, () => console.log(`Running server on ${hostname}:${port}`))
-
-app.get('/', (req, res) => {
-    res.send('Hello world!');
+app.listen(port, () => {
+    // logger.info(`App is running at http://${hostname}:${port}`)
+    console.log('test')
+    logger.info('new test')
 
 }
 )
+routes(app);
+
+
+export default app;
