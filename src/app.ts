391853681c4ab297c590ea7175c6eab33128ed00
Ido <idoh52@gmail.com>
2022-01-22 01:50:10 +0200

    adding config package and moving port number to there

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import routes from './routes';
 import logger from './utils/logger';
-const port = 3001;
-const hostname = 'localhost';
+import config from "config";
+
+const port = config.get<number>('port');
 
 const app = express();
 // app.use(express.json());
 
 app.listen(port, () => {
-    logger.info(`App is running at http://${hostname}:${port}`)
+    logger.info(`App is running at http://hostname:${port}`)
 
 }
 )
