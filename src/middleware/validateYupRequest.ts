2b572df8407e338e0f7066f529209f0df781fc9e
Ido <idoh52@gmail.com>
2022-01-23 00:51:39 +0200

    init yup middleware to validate user input

---
+++ src/middleware/validateYupRequest.ts
@@ -0,0 +1,37 @@
+import { object, string, number, date, AnyObjectSchema } from "yup";
+import { NextFunction, Request, Response } from "express";
+import logger from "../utils/logger";
+
+export const ValidateYup = (schema: AnyObjectSchema) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = await schema.validate(req.body);
+
+      logger.info(
+        `validated successfully, validate data:  ${JSON.stringify(data)}`
+      );
+
+      next();
+    } catch (error) {
+      logger.error(`didn't validate, error: ${error}`);
+
+      return res.status(422).json({ error });
+    }
+  };
+};
+
+export const Schemas = {
+  user: object().shape({
+    username: string().required(),
+  }),
+  exercise: object().shape({
+    username: string().required(),
+    duration: number().required().positive().integer(),
+    description: string().required(),
+    date: date().default(() => new Date()),
+  }),
+};
+
+export interface userInputInterface {
+  username: string;
+}
