2f4352ba12ff82ab9d50cea999dc87e1a3bb7821
Ido <idoh52@gmail.com>
2022-01-22 16:18:02 +0200

    adding TODO for schema design

--- src/utils/validateSchema.ts
+++ src/utils/validateSchema.ts
@@ -3,7 +3,7 @@ import config from 'config'
 interface SchemaInterface {
     [key: string]: any
 }
-
+// TODO change the inputType to array and then add few regex ( for parsing every date string)
 const schema: SchemaInterface = {
     date: {
         unixtimeStampInputType: "^[0-9]+$", dateStringInputType: "^([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$", emptyInputType: "^$"
