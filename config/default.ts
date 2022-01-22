a47b0a9aa90ef2878dfb9d671f01e65528e6e1ca
Ido <idoh52@gmail.com>
2022-01-22 03:01:38 +0200

    init config file

---
+++ config/default.ts
@@ -0,0 +1,7 @@
+export default {
+  port: 3000,
+  unknownUserInputType: 'Unkwon User Input Type',
+  unixtimeStampInputType: 'unixtimeStampInputType',
+  dateStringInputType: 'dateStringInputType',
+  emptyInputType: 'emptyInputType'
+};
