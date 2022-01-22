c3b03786b0e2184dab69dd84c95be1aa6c0ac470
Ido <idoh52@gmail.com>
2022-01-22 23:06:32 +0200

    Adding db connection to app

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,17 @@
-import express from 'express';
-import routes from './routes';
-import logger from './utils/logger';
+import express from "express";
+import routes from "./routes";
+import logger from "./utils/logger";
 import config from "config";
-
-const port = config.get<number>('port');
+import mongoConnector from "./utils/mongoConnector";
+const port = config.get<number>("port");
 
 const app = express();
-// app.use(express.json());
+app.use(express.json());
 
-app.listen(port, () => {
-    logger.info(`App is running at http://hostname:${port}`)
-
-}
-)
-routes(app);
-
+app.listen(port, async () => {
+  logger.info(`App is running at http://hostname:${port}`);
+  await mongoConnector();
+  routes(app);
+});
 
 export default app;
