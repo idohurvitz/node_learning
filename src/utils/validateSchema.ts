e80d04a6734b1bbad40d3c0f1d12db03a7bef9d2
Ido <idoh52@gmail.com>
2022-01-22 02:58:28 +0200

    adding new inputType - empty param

--- src/utils/validateSchema.ts
+++ src/utils/validateSchema.ts
@@ -1,10 +1,12 @@
+import config from 'config'
+
 interface SchemaInterface {
     [key: string]: any
 }
 
 const schema: SchemaInterface = {
     date: {
-        unix_timestamp: "^[0-9]+$", date_string: "^([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$"
+        unixtimeStampInputType: "^[0-9]+$", dateStringInputType: "^([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$", emptyInputType: "^$"
     }
 }
 
