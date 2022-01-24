4af6d50557109e393c42fc23bd6f33b26f8dc9ab
Ido <idoh52@gmail.com>
2022-01-24 21:28:48 +0200

    making date propery a requierd

--- src/models/exercise.model.ts
+++ src/models/exercise.model.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Document, Schema } from 'mongoose';
 
 interface ExerciseInterface extends Document {
   username: string;
@@ -9,11 +9,11 @@ const ExerciseSchema: Schema = new Schema(
     userId: { type: String, required: true },
     duration: { type: Number, required: true },
     description: { type: String, required: true },
-    date: { type: Date, required: false },
+    date: { type: Date, required: true }
   },
   {
-    timestamps: false, // for the exercise design will implements my self
+    timestamps: false // for the exercise design will implements my self
   }
 );
 
-export default mongoose.model<ExerciseInterface>("Exercise", ExerciseSchema);
+export default mongoose.model<ExerciseInterface>('Exercise', ExerciseSchema);
