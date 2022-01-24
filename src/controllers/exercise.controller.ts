097fd2d86630e1a1632bbbfeb80dbdf3803deb93
Ido <idoh52@gmail.com>
2022-01-24 23:04:56 +0200

    adding get exerciseByUserId middleware (without params handling)

--- src/controllers/exercise.controller.ts
+++ src/controllers/exercise.controller.ts
@@ -5,6 +5,7 @@ import logger from '../utils/logger';
 import { exerciseInputInterface } from '../middleware/validateYupRequest';
 const createExercise = (req: Request, res: Response, next: NextFunction) => {
   // I added the user object to the request headers
+  // I tried to using mongo middleware to return the Date object as a string instead of wrapping it, will explain to you personally :)
   const excerciseObj: exerciseInputInterface = req.body; // here i can assume the req.body passed the validation and therefore i can predict the type (will be used later)
   logger.info(`got new request for creating exercise, request body: ${JSON.stringify(req.body)}`);
 
@@ -23,7 +24,7 @@ const createExercise = (req: Request, res: Response, next: NextFunction) => {
   const exercise = new Exercise({ ...userId, ...excerciseObj }); // concat more then two objects (bloody ecma6)
 
   return exercise
-    .save()
+    .save() // TODO switch to use await instead of .function
     .then((result: any) => {
       logger.info(
         `adding exercise successfully : ${JSON.stringify({
@@ -51,4 +52,32 @@ const createExercise = (req: Request, res: Response, next: NextFunction) => {
     });
 };
 
-export default { createExercise };
+const GetExerciseByUserId = async (req: Request, res: Response, next: NextFunction) => {
+  const currentUser = req.CurrentUserObject;
+  if (Object.entries(currentUser).length === 0) {
+    logger.error(
+      `get exercise by user id request | no user in headers, you probably didn't config the middleware's good, the user object is:${JSON.stringify(
+        req.CurrentUserObject
+      )}`
+    );
+    return res.status(500).json({ error: 'error message' });
+  }
+  logger.info(
+    `get exercise by user id request | getting user from request headers, user object: ${JSON.stringify(req.CurrentUserObject)}`
+  );
+
+  try {
+    const exercises = await Exercise.find({ userId: currentUser._id }).exec();
+
+    logger.info(`returning exercises of user :${currentUser._id}, amount of exercises: ${exercises.length}`);
+    return res.status(200).json({ ...currentUser, log: exercises, count: exercises.length });
+  } catch (error: any) {
+    logger.error(`adding exercise failed, error message: ${error.message}`);
+    return res.status(500).json({
+      message: error.message,
+      error
+    });
+  }
+};
+
+export default { createExercise, GetExerciseByUserId };
