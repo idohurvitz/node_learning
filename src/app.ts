84eb4f4b238f98f07f444eeb16ccf1e4553b0511
Ido <idoh52@gmail.com>
2022-01-25 21:48:55 +0200

    adding urlencoded middleware to handle free code camp tests

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,10 @@ import mongoConnector from './utils/mongoConnector';
 const cors = require('cors');
 
 const port = config.get<number>('port');
-
 const app = express();
-app.use(express.json());
 app.use(cors());
-
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded -- freecodecamp tests
 app.listen(port, async () => {
   logger.info(`App is running at http://hostname:${port}`);
   await mongoConnector();
