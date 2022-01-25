5fb48f4155db752b259bff4040d1dc48c4915215
Ido <idoh52@gmail.com>
2022-01-25 21:47:41 +0200

    prettier default.ts

--- config/default.ts
+++ config/default.ts
@@ -1,12 +1,12 @@
-const mongoUserName: string = "admin";
-const mongoUserPassword: string = "Password1";
+const mongoUserName: string = 'admin';
+const mongoUserPassword: string = 'Password1';
 
 export default {
   port: 3000,
-  unknownCurrentUserInputType: "Unknown User Input Type",
-  unixtimeStampInputType: "unixtimeStampInputType",
-  dateStringInputType: "dateStringInputType",
-  emptyInputType: "emptyInputType",
-  invalidDateError: "Invalid Date",
-  dbUri: `mongodb+srv://${mongoUserName}:${mongoUserPassword}@node-learning.hopqk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  unknownCurrentUserInputType: 'Unknown User Input Type',
+  unixtimeStampInputType: 'unixtimeStampInputType',
+  dateStringInputType: 'dateStringInputType',
+  emptyInputType: 'emptyInputType',
+  invalidDateError: 'Invalid Date',
+  dbUri: `mongodb+srv://${mongoUserName}:${mongoUserPassword}@node-learning.hopqk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 };
