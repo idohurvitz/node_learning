ccbc60b7c7ea917a911e69fd90350c1773a3e070
Ido <idoh52@gmail.com>
2022-01-25 20:24:00 +0200

    adding cors package

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
-import express from "express";
-import routes from "./routes";
-import logger from "./utils/logger";
-import config from "config";
-import mongoConnector from "./utils/mongoConnector";
-const port = config.get<number>("port");
+import express from 'express';
+import routes from './routes';
+import logger from './utils/logger';
+import config from 'config';
+import mongoConnector from './utils/mongoConnector';
+const cors = require('cors');
+
+const port = config.get<number>('port');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.listen(port, async () => {
   logger.info(`App is running at http://hostname:${port}`);
