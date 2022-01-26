dfac5d4eb381077e6d85cb57702d2196a971cdeb
Ido <idoh52@gmail.com>
2022-01-26 20:18:16 +0200

    moving schemas to new separate file

--- src/middleware/validateYupRequest.ts
+++ src/middleware/validateYupRequest.ts
@@ -1,4 +1,4 @@
-import { object, string, number, date, AnyObjectSchema } from 'yup';
+import { AnyObjectSchema } from 'yup';
 import { NextFunction, Request, Response } from 'express';
 import logger from '../utils/logger';
 
@@ -17,35 +17,9 @@ export const ValidateYup = (schema: AnyObjectSchema, type: 'body' | 'headers') =
       type === 'body' ? (req.body = inputPostValidation) : '';
       next();
     } catch (error) {
-      logger.error(`Validator | didn't validate | user input was: ${JSON.stringify(validatedInput)} | error: ${error} | tr`);
+      logger.error(`Validator | didn't validate | user input was: ${JSON.stringify(validatedInput)} | error: ${error} `);
 
       return res.status(422).json({ error });
     }
   };
 };
-// TODO using the typescript way of yup (ObjectSchema)
-export const Schemas = {
-  userBodySchema: object().shape({
-    username: string().required()
-  }),
-  exerciseBodySchema: object().shape({
-    duration: number().required().positive().integer(),
-    description: string().required(),
-    date: date().default(() => new Date())
-  }),
-  exerciseParamsSchema: object().shape({
-    from: string().matches(new RegExp('^([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$')),
-    to: string().matches(new RegExp('^([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$')),
-    limit: number()
-  })
-};
-
-export interface userInputInterface {
-  username: string;
-}
-
-export interface exerciseInputInterface {
-  duration: number;
-  description: string;
-  date: Date;
-}
