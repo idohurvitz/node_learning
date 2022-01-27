5a168bdd547da60eefb92e6c30ef20af1df7d554
Ido <idoh52@gmail.com>
2022-01-27 18:39:43 +0200

    running prettier on project and fix typoes

--- src/utils/mongoConnector.ts
+++ src/utils/mongoConnector.ts
@@ -5,7 +5,7 @@ async function connect() {
   const dbUri = config.get<string>('dbUri');
 
   try {
-    const connection = await mongoose.connect(dbUri);
+    await mongoose.connect(dbUri);
     logger.info('DB connected | dburi: ' + dbUri);
   } catch (error: any) {
     logger.error(`Could not connect to db | error: ${error} | error message: ${error.message}`);
--- src/utils/validateSchema.ts
+++ src/utils/validateSchema.ts
@@ -1,14 +1,15 @@
-import config from 'config'
+import config from 'config';
 
 interface SchemaInterface {
-    [key: string]: any
+  [key: string]: any;
 }
 // TODO change the inputType to array and then add few regex ( for parsing every date string)
 const schema: SchemaInterface = {
-    date: {
-        unixtimeStampInputType: "^[0-9]+$", dateStringInputType: "^([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$", emptyInputType: "^$"
-    }
-}
+  date: {
+    unixtimeStampInputType: '^[0-9]+$',
+    dateStringInputType: '^([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$',
+    emptyInputType: '^$'
+  }
+};
 
-
-export default schema
+export default schema;
