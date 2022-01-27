585a57a07db865b46babec60f6ee8c99d2a4b850
Ido <idoh52@gmail.com>
2022-01-27 19:05:45 +0200

    updating to ES7 async/await syntax

--- src/controllers/url.controller.ts
+++ src/controllers/url.controller.ts
@@ -19,40 +19,35 @@ const createUrl = async (req: Request, res: Response, next: NextFunction) => {
   }
   const uniqueId = { _id: new mongoose.Types.ObjectId() };
   const url = new Url({ ...uniqueId, ...urlObj }); // concat more then two objects (bloody ecma6)
-
-  return url
-    .save()
-    .then((resultObj: any) => {
-      logger.info(`adding url successfully : ${JSON.stringify(resultObj)}`);
-      return res.status(201).json({ original_url: resultObj.url, short_url: resultObj.short_url });
-    })
-    .catch((error: any) => {
-      logger.error(`adding url failed, error message: ${error.message}`);
-      return res.status(500).json({
-        message: error.message,
-        error
-      });
+  try {
+    const resultObj = await url.save();
+    logger.info(`adding url successfully : ${JSON.stringify(resultObj)}`);
+    return res.status(201).json({ original_url: resultObj.url, short_url: resultObj.short_url });
+  } catch (error: any) {
+    logger.error(`adding url failed, error message: ${error.message}`);
+    return res.status(500).json({
+      message: error.message,
+      error
     });
+  }
 };
 
-const getUrl = (req: Request, res: Response, next: NextFunction) => {
+const getUrl = async (req: Request, res: Response, next: NextFunction) => {
   logger.info(`got new request for handling url, requesrt params: ${JSON.stringify(req.params)}`);
   logger.info(`shorturl type is: ${typeof req.params}`);
   const urlId = parseInt(req.params.shortUrl);
-  Url.find({ short_url: urlId })
-    .exec()
-    .then((result: any) => {
-      logger.info(`redirecting url, url tuple:${result[0]}`);
-      res.redirect(result[0].url);
-      next();
-      return;
-    })
-    .catch((error: any) => {
-      logger.error(`returning url failed, error message: ${error.message}`);
-      return res.status(500).json({
-        message: error.message,
-        error
-      });
+  try {
+    const resultObj = await Url.find({ short_url: urlId }).exec();
+    logger.info(`redirecting url, url tuple:${resultObj[0]}`);
+    res.redirect(resultObj[0].url);
+    next();
+    return;
+  } catch (error: any) {
+    logger.error(`returning url failed, error message: ${error.message}`);
+    return res.status(500).json({
+      message: error.message,
+      error
     });
+  }
 };
 export default { createUrl, getUrl };
